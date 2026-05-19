2e29244184a519b03a7e2bdba16b7339f061cc08
LidiiaChyrnyshuk <l.mandych@gmail.com>
2026-05-19 09:21:02 +0200

    update body background

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -3,10 +3,10 @@ import styled from "@emotion/styled";
 export const MainContainer = styled.div`
 	display: grid;
 	gap: 0;
-	width:100%;
+	width: 100%;
 	max-width: 1200px;
 	margin: 0 auto;
 	padding: 0;
 	border: 0;
-	
+	background: #ffffff;
 `;
